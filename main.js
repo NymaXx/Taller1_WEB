513c217bfdfe62371282c38c41e1e285f9b6bf31
NymaXx <53951684+NymaXx@users.noreply.github.com>
2021-03-14 22:09:19 -0500

    carrusel de la interaccion pdf colocado, hay problema con la posicion, no se arregla con margenes pendiente revisar

--- main.js
+++ main.js
@@ -10,5 +10,19 @@ function handleInterval(){
     slyder.style.transform = `translate(-${ 300 * currentSlide }px, 0px)`;
 }
 
-setInterval(handleInterval, 1000);
+setInterval(handleInterval, 2500);
+
+
+const pdfSlyder = document.querySelector('.description__slyder');
+let currentPdfSlide = 0;
+function handlePdfInterval(){
+    currentPdfSlide++;
+    if(currentPdfSlide >=8){
+        currentPdfSlide =0;
+    }
+    pdfSlyder.style.transform = `translate(-${ 194 * currentPdfSlide }px, 0px)`;
+    console.log("sirve");
+}
+
+setInterval(handlePdfInterval, 2500);
 
