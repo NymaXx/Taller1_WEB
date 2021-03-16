e798a4ac145b8aabd76c244ce54938ed006c575a
NymaXx <53951684+NymaXx@users.noreply.github.com>
2021-03-16 17:52:55 -0500

    modal implementado con animacion falta salir del modal

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Galery carrousel
+
 const slyder = document.querySelector('.galery__slyder');
 let currentSlide = 0;
 
@@ -12,6 +14,7 @@ function handleInterval(){
 
 setInterval(handleInterval, 2500);
 
+//Description pdf interaction
 
 const pdfSlyder = document.querySelector('.description__slyder');
 let currentPdfSlide = 0;
@@ -26,3 +29,31 @@ function handlePdfInterval(){
 
 setInterval(handlePdfInterval, 2500);
 
+
+
+//to Modal interaction
+
+const btnOpenModal = document.querySelector('.btninteract');
+const modal = document.querySelector('.modal');
+
+function handleOpenModal () {
+  modal.style.display = 'block';
+  document.body.style.overflow = 'hidden';
+}
+
+btnOpenModal.addEventListener('click', handleOpenModal);
+
+//modal animation
+
+const modalSlyder = document.querySelector('.modal__slyder');
+let currentModalSlide = 0;
+function handlePdfInterval(){
+    currentModalSlide++;
+    if(currentModalSlide >=8){
+        currentModalSlide =0;
+    }
+    modalSlyder.style.transform = `translate(0px, -${ 239 * currentModalSlide }px)`;
+    console.log("sfunciona");
+}
+
+setInterval(handlePdfInterval, 2500);
