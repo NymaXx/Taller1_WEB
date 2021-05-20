313bd2de36971101a592684997bfb70f885e7abd
NymaXx <53951684+NymaXx@users.noreply.github.com>
2021-05-19 21:21:24 -0500

    correcciones a anadir a la tienda, todos los elementos subidos y obtenidos

--- JS/productForm.js
+++ JS/productForm.js
@@ -1,16 +1,3 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBc--wcJHq3qJnXF-R75pn2Wl963Q_iAGM",
-    authDomain: "noona-webstore-211.firebaseapp.com",
-    projectId: "noona-webstore-211",
-    storageBucket: "noona-webstore-211.appspot.com",
-    messagingSenderId: "194192080159",
-    appId: "1:194192080159:web:3d49ec0891952697a6442a"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-
-const db = firebase.firestore();
-const storage = firebase.storage();
 
 const productForm = document.querySelector('.productForm');
 const productImg = document.querySelector('.productForm__img');
--- JS/store.js
+++ JS/store.js
@@ -1,89 +1,46 @@
-const products = [
-    {
-        img: '../imgStore/milveceshastasiempre.jpg',
-        title: 'MIL VECES HASTA SIEMPRE',
-        autor: 'Jhon Green',
-        precio: 32000,
-        category: 'Juvenil',
-        idioma: 'Español',
-    },
-
-    {
-        img: '../imgStore/milveceshastasiempre.jpg',
-        title: 'MIL VECES HASTA SIEMPRE',
-        autor: 'Jhon Green',
-        precio: 32000,
-        category: 'Juvenil',
-        idioma: 'Español',
-    },
-
-    {
-        img: '../imgStore/milveceshastasiempre.jpg',
-        title: 'MIL VECES HASTA SIEMPRE',
-        autor: 'Jhon Green',
-        precio: 32000,
-        category: 'Juvenil',
-        idioma: 'Español',
-    },
-
-    {
-        img: '../imgStore/milveceshastasiempre.jpg',
-        title: 'MIL VECES HASTA SIEMPRE',
-        autor: 'Jhon Green',
-        precio: 32000,
-        category: 'Juvenil',
-        idioma: 'Español',
-    },
-
-    {
-        img: '../imgStore/milveceshastasiempre.jpg',
-        title: 'MIL VECES HASTA SIEMPRE',
-        autor: 'Jhon Green',
-        precio: 32000,
-        category: 'Juvenil',
-        idioma: 'Español',
-    }
-
-];
-
 
 const list = document.querySelector('.list');
 
-function handleProductItem (item) {
-    const basicProd = document.createElement('article');
-    basicProd.innerHTML = `
-        <a class="basicProd__imgCont" href="../html/productView.html">
-            <img class="basicProd__img" src="${item.img}" alt="" >   
-        </a>
+db.collection("products")
+    .get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            const data = doc.data();
 
-        <div class="basicProd__info">
-            <div class="basicProd__title">
-                ${item.title}
-            </div>
-            <div class="basicProd__autor">
-                Autor: <strong> ${item.autor}</strong> 
-            </div>
-            <div class="basicProd__precio">
-                $ ${item.precio}
-            </div>
-            <div class="basicProd__category">
-                Categoria: <strong> ${item.category} </strong>
-            </div>
-            <div class="basicProd__idioma">
-                Idioma: <strong> ${item.idioma} </strong> 
-            </div>
-        </div>
-        <div class="basicProd__buttons">
-            <button class="basicProd__add">Añadir al carrito</button>
-            <a href="../html\productView.html">
-                <button class="basicProd__ver">Ver detalles</button>
-            </a> 
-        </div>
-    `
+            const basicProd = document.createElement('article');
+            basicProd.innerHTML = `
+                <a class="basicProd__imgCont" href="../html/productView.html">
+                    <img class="basicProd__img" src="${data.imageUrl}" alt="" >   
+                </a>
+        
+                <div class="basicProd__info">
+                    <div class="basicProd__title">
+                        ${data.name}
+                    </div>
+                    <div class="basicProd__autor">
+                        Autor: <strong> ${data.autor}</strong> 
+                    </div>
+                    <div class="basicProd__precio">
+                        $ ${data.precio}
+                    </div>
+                    <div class="basicProd__category">
+                        Categoria: <strong> ${data.categoria} </strong>
+                    </div>
+                    <div class="basicProd__idioma">
+                        Idioma: <strong> ${data.idioma} </strong> 
+                    </div>
+                </div>
+                <div class="basicProd__buttons">
+                    <button class="basicProd__add">Añadir al carrito</button>
+                    <a href="../html/productView.html">
+                        <button class="basicProd__ver">Ver detalles</button>
+                    </a> 
+                </div>
+            `
+        
+            basicProd.classList.add('basicProd');
+            list.appendChild(basicProd);
 
-    basicProd.classList.add('basicProd');
+        });
+    })
 
-    list.appendChild(basicProd);
-}
-
-products.forEach(handleProductItem)
